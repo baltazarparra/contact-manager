c2bd181e18298d7ed4a4b087e15f5b06861d9236
baltazarparra <baltazarparra@outlook.com>
2018-03-06 07:05:58 -0300

    Integrar aplicação com firebase e validar integração

--- src/app/components/auth/auth.module.js
+++ src/app/components/auth/auth.module.js
@@ -2,4 +2,20 @@ angular
   .module('components.auth', [
     'ui.router',
     'firebase'
-  ]);
+  ])
+  .config(function ($firebaseRefProvider) {
+    var config = {
+      apiKey: "AIzaSyCNXe3i2lflgUwtfgnvItR1LuWQOPHJvmI",
+      authDomain: "contacts-manager-f33e8.firebaseapp.com",
+      databaseURL: "https://contacts-manager-f33e8.firebaseio.com",
+      projectId: "contacts-manager-f33e8",
+      storageBucket: "contacts-manager-f33e8.appspot.com",
+      messagingSenderId: "288648125855"
+    };
+    $firebaseRefProvider
+      .registerUrl({
+        default: config.databaseURL,
+        contacts: config.databaseURL + '/contacts'
+      });
+    firebase.initializeApp(config);
+  });
