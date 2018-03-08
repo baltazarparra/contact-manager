56ece2dd8e3b4b29438f1f3eb4bffeff2e3f8192
baltazarparra <baltazarparra@outlook.com>
2018-03-08 08:19:50 -0300

    Implementar service de autenticação e logica de registro e login

--- src/app/components/auth/login/login.controller.js
+++ src/app/components/auth/login/login.controller.js
@@ -1,5 +1,21 @@
-function LoginController() {
+function LoginController(AuthService) {
   const ctrl = this
+  ctrl.$onInit = () => {
+    ctrl.error = null
+    ctrl.user = {
+      email: '',
+      password: ''
+    }
+  }
+  ctrl.loginUser = (event) => {
+    return AuthService
+      .login(event.user)
+      .then(function () {
+        console.log('Success Login!')
+      }, function(reason) {
+        ctrl.error =  reason.message
+      })
+  }
 }
 
 angular
--- src/app/components/auth/register/register.controller.js
+++ src/app/components/auth/register/register.controller.js
@@ -1,5 +1,21 @@
-function RegisterController() {
+function RegisterController(AuthService) {
   const ctrl = this
+  ctrl.$onInit = () => {
+    ctrl.error = null
+    ctrl.user = {
+      email: '',
+      password: ''
+    }
+  }
+  ctrl.createUser = (event) => {
+    return AuthService
+      .register(event.user)
+      .then(function () {
+        console.log('Success Register!')
+      }, function(reason) {
+        ctrl.error =  reason.message
+      })
+  }
 }
 
 angular
