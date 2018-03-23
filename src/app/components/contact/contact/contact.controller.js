1dad21cb77bef9d6b339693241684893e9b46ad0
baltazarparra <baltazarparra@outlook.com>
2018-03-23 07:10:20 -0300

    Implementar detalhamento de contato

---
+++ src/app/components/contact/contact/contact.controller.js
@@ -0,0 +1,14 @@
+function ContactController () {
+  const ctrl = this
+  ctrl.selectContact = function() {
+    ctrl.onSelect({
+      $event: {
+        contactId: ctrl.contact.$id
+      }
+    })
+  }
+}
+
+angular
+  .module('components.contact')
+  .controller('ContactController', ContactController)
